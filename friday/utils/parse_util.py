e1d3aade2f95c7286f0e09add94753201be1589b
randheerrrk <randheerrrk@gmail.com>
2021-01-22 16:22:29 +0530

    added parse_yaml

--- friday/utils/parse_util.py
+++ friday/utils/parse_util.py
@@ -1,21 +1,44 @@
 import json
+import yaml
+from yaml import YAMLError, scanner
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 
 def parse_xml(path) :
     pass
 
 def parse_json(path) :
-    try:
-        with open(path) as f:
-            data = json.load(f)
-            return data
-    except FileNotFoundError as err:
-        return err
-    except PermissionError as err:
-        return err
-    except json.decoder.JSONDecodeError:
-        return "JSONDecodeError"
+    entitiySetDetails = None
+    err = None
+    try :
+        with open(path) as stream :
+            entitiySetDetails = json.load(stream)
+    except FileNotFoundError:
+        err = FileNotFoundError
+    except PermissionError:
+        err = PermissionError
+    except json.decoder.JSONDecodeError as JSONDecodeError:
+        err = JSONDecodeError
     except:
-        return "error"
+        err = "Unable to parse the file"
+
+    return entitiySetDetails, err
+
 
 def parse_yaml(path) :
-    pass
+    entitiySetDetails = None
+    err = None
+    try :
+        with open(path) as stream :
+            entitiySetDetails = yaml.load(stream, Loader=Loader)
+    except PermissionError:
+        err = PermissionError
+    except FileNotFoundError:
+        err = FileNotFoundError
+    except YAMLError: 
+        err = YAMLError
+    except :
+        err = "Unable to parse the file"
+    return entitiySetDetails, err
