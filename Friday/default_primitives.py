10a162da5823c10c25c11953bf6204789cbcf7c4
Denil C Verghese <denilverghese@gmail.com>
2021-05-10 08:51:57 +0530

    sanitise the repo to final form

--- Friday/default_primitives.py
+++
@@ -1,8 +0,0 @@
-from unit_transformations import log_transfom, reciprocal_transform, sqrt_transform, power_transform
-
-DEFAULT_PRIMITIVES = {
-    "log_transfom" : log_transfom,
-    "reciprocal_transform" : reciprocal_transform,
-    "sqrt_transform" : sqrt_transform,
-    "power_transform" : power_transform
-}
